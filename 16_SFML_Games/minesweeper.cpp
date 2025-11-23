18ca840df0c19792c81547133452341f04a8bbd5
Sofiia Zemlianska <C00313181@setu.ie>
2025-11-23 20:48:44 +0000

    refactor:remove magic numbers

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -6,37 +6,46 @@ int minesweeper()
 {
     srand(time(0));
 
-    RenderWindow window(VideoMode(400, 400), "Minesweeper!");
+    const int GRID_SIZE = 10;
+    const int BUFFER_SIZE = 12;
+    const int CELL_SIZE = 32;
+    const int WINDOW_SIZE = 400;
+    const int MINE_VALUE = 9;
+    const int HIDDEN_CELL = 10;
+    const int FLAG_CELL = 11;
+    const int MINE_PROBABILITY = 5;
 
-    int cellSize = 32;
-    int mineGrid[12][12];
-    int visibleGrid[12][12]; //for showing
+    RenderWindow window(VideoMode(WINDOW_SIZE, WINDOW_SIZE), "Minesweeper!");
+
+    int cellSize = CELL_SIZE;
+    int mineGrid[BUFFER_SIZE][BUFFER_SIZE];
+    int visibleGrid[BUFFER_SIZE][BUFFER_SIZE]; //for showing
 
     Texture texture;
     texture.loadFromFile("images/minesweeper/tiles.jpg");
     Sprite sprite(texture);
 
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
+    for (int i=1;i<=GRID_SIZE;i++)
+     for (int j=1;j<=GRID_SIZE;j++)
       {
-        visibleGrid[i][j]=10;
-        if (rand()%5==0)  mineGrid[i][j]=9;
+        visibleGrid[i][j]=HIDDEN_CELL;
+        if (rand()%MINE_PROBABILITY==0)  mineGrid[i][j]=MINE_VALUE;
         else mineGrid[i][j]=0;
       }
 
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
+    for (int i=1;i<=GRID_SIZE;i++)
+     for (int j=1;j<=GRID_SIZE;j++)
       {
         int adjacentMines=0;
-        if (mineGrid[i][j]==9) continue;
-        if (mineGrid[i+1][j]==9) adjacentMines++;
-        if (mineGrid[i][j+1]==9) adjacentMines++;
-        if (mineGrid[i-1][j]==9) adjacentMines++;
-        if (mineGrid[i][j-1]==9) adjacentMines++;
-        if (mineGrid[i+1][j+1]==9) adjacentMines++;
-        if (mineGrid[i-1][j-1]==9) adjacentMines++;
-        if (mineGrid[i-1][j+1]==9) adjacentMines++;
-        if (mineGrid[i+1][j-1]==9) adjacentMines++;
+        if (mineGrid[i][j]==MINE_VALUE) continue;
+        if (mineGrid[i+1][j]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i][j+1]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i-1][j]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i][j-1]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i+1][j+1]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i-1][j-1]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i-1][j+1]==MINE_VALUE) adjacentMines++;
+        if (mineGrid[i+1][j-1]==MINE_VALUE) adjacentMines++;
         mineGrid[i][j]=adjacentMines;
       }
 
@@ -54,15 +63,15 @@ int minesweeper()
 
             if (event.type == Event::MouseButtonPressed)
               if (event.key.code == Mouse::Left) visibleGrid[x][y]=mineGrid[x][y];
-              else if (event.key.code == Mouse::Right) visibleGrid[x][y]=11;
+              else if (event.key.code == Mouse::Right) visibleGrid[x][y]=FLAG_CELL;
         }
 
         window.clear(Color::White);
 
-        for (int i=1;i<=10;i++)
-         for (int j=1;j<=10;j++)
+        for (int i=1;i<=GRID_SIZE;i++)
+         for (int j=1;j<=GRID_SIZE;j++)
           {
-           if (visibleGrid[x][y]==9) visibleGrid[i][j]=mineGrid[i][j];
+           if (visibleGrid[x][y]==MINE_VALUE) visibleGrid[i][j]=mineGrid[i][j];
            sprite.setTextureRect(IntRect(visibleGrid[i][j]*cellSize,0,cellSize,cellSize));
            sprite.setPosition(i*cellSize, j*cellSize);
            window.draw(sprite);
